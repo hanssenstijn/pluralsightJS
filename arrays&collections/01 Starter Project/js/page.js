56979532c58ac650e395e77bf6d9f71be48787aa
hanssenstijn <hanssenstijn@gmail.com>
2020-06-03 12:05:34 +0200

    chart.js, visualize revenues

---
+++ arrays&collections/01 Starter Project/js/page.js
@@ -0,0 +1,70 @@
+// Charts
+let ctx = document.getElementById('monthlySales').getContext('2d');
+let pieCtx = document.getElementById('deptSales').getContext('2d');
+let yearlyLabel = document.getElementById('yearlyTotal');
+
+let monthlySales = Array.of(1200, 9000, 3000);
+let monthlyLabels = Array.of('Oct', 'Nov', 'Dec');
+
+let deptSales = Array.of(12, 9, 3);
+let deptLabels = Array.of('Hiking', 'Running', 'Hunting');
+
+function addYearlyTotal(a, b, c) {
+  return a + b + c;
+}
+
+let octNums = Array.of(12000, 1000, 9000);
+let novNums = Array.of(1000, 2000, 8000);
+let decNums = Array.of(2000, 3000, 4000);
+
+let total = Array.of(addYearlyTotal(...octNums), addYearlyTotal(...novNums), addYearlyTotal(...decNums));
+
+let yearlyTotal = addYearlyTotal(...monthlySales);
+yearlyLabel.innerHTML = "$" + yearlyTotal;
+// Bar
+var monthlySalesChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: monthlyLabels,
+    datasets: [{
+      label: '# of Sales',
+      data: monthlySales,
+      backgroundColor: [
+        'rgba(238, 184, 104, 1)',
+        'rgba(75, 166, 223, 1)',
+        'rgba(239, 118, 122, 1)',
+      ],
+      borderWidth: 0
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
+
+// Pie
+var deptSalesChart = new Chart(pieCtx, {
+  type: 'pie',
+  data: {
+    labels: deptLabels,
+    datasets: [{
+      label: '# of Sales',
+      data: deptSales,
+      backgroundColor: [
+        'rgba(238, 184, 104, 1)',
+        'rgba(75, 166, 223, 1)',
+        'rgba(239, 118, 122, 1)',
+      ],
+      borderWidth: 0
+    }]
+  },
+  options: {
+
+  }
+})
