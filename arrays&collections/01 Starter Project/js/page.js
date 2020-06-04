4dafb2e2453994f7df3ad028b25c579c9420e930
hanssenstijn <hanssenstijn@gmail.com>
2020-06-04 16:51:31 +0200

    working with arrays and sets

--- arrays&collections/01 Starter Project/js/page.js
+++ arrays&collections/01 Starter Project/js/page.js
@@ -1,70 +1,92 @@
 // Charts
 let ctx = document.getElementById('monthlySales').getContext('2d');
 let pieCtx = document.getElementById('deptSales').getContext('2d');
+
 let yearlyLabel = document.getElementById('yearlyTotal');
 
-let monthlySales = Array.of(1200, 9000, 3000);
-let monthlyLabels = Array.of('Oct', 'Nov', 'Dec');
+let newAmount = document.getElementById('idemAmount')
+let newMonth = document.getElementById('monthId');
 
-let deptSales = Array.of(12, 9, 3);
-let deptLabels = Array.of('Hiking', 'Running', 'Hunting');
+// let monthlySales = Array.of(500, 9000, 3000, 4000);
+// let monthlyLabels = Array.of('Oct', 'Nov', 'Dec');
 
-function addYearlyTotal(a, b, c) {
-  return a + b + c;
-}
+// let deptSales = Array.of(12, 9, 3);
+// let deptLabels = Array.of('Hiking', 'Running', 'Hunting');
 
-let octNums = Array.of(12000, 1000, 9000);
-let novNums = Array.of(1000, 2000, 8000);
-let decNums = Array.of(2000, 3000, 4000);
+let yearlyTotal = 0;
 
-let total = Array.of(addYearlyTotal(...octNums), addYearlyTotal(...novNums), addYearlyTotal(...decNums));
+const monthlySales = new Set();
+const monthlyLabels = new Set();
 
-let yearlyTotal = addYearlyTotal(...monthlySales);
-yearlyLabel.innerHTML = "$" + yearlyTotal;
-// Bar
-var monthlySalesChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: monthlyLabels,
-    datasets: [{
-      label: '# of Sales',
-      data: monthlySales,
-      backgroundColor: [
-        'rgba(238, 184, 104, 1)',
-        'rgba(75, 166, 223, 1)',
-        'rgba(239, 118, 122, 1)',
-      ],
-      borderWidth: 0
-    }]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true
-        }
-      }]
-    }
-  }
-});
 
-// Pie
-var deptSalesChart = new Chart(pieCtx, {
-  type: 'pie',
-  data: {
-    labels: deptLabels,
-    datasets: [{
-      label: '# of Sales',
-      data: deptSales,
-      backgroundColor: [
-        'rgba(238, 184, 104, 1)',
-        'rgba(75, 166, 223, 1)',
-        'rgba(239, 118, 122, 1)',
-      ],
-      borderWidth: 0
-    }]
-  },
-  options: {
+// function addYearlyTotal(x) {
+//   yearlyTotal = x + yearlyTotal;
+// }
 
-  }
-})
+// monthlySales.forEach(addYearlyTotal);
+
+// let octNums = Array.of(12000, 1000, 9000);
+// let novNums = Array.of(1000, 2000, 8000);
+// let decNums = Array.of(2000, 3000, 4000);
+
+// // let total = Array.of(addYearlyTotal(...octNums), addYearlyTotal(...novNums), addYearlyTotal(...decNums));
+
+// yearlyLabel.innerHTML = "$" + yearlyTotal;
+
+// function findOver1000() {
+//   let firstThousand = monthlySales.findIndex(element => element > 1000);
+//   alert(firstThousand);
+// }
+
+// function resetNum() {
+//   monthlySales.fill(0);
+//   monthlySalesChart.update();
+// }
+
+// // Bar
+// var monthlySalesChart = new Chart(ctx, {
+//   type: 'bar',
+//   data: {
+//     labels: monthlyLabels,
+//     datasets: [{
+//       label: '# of Sales',
+//       data: monthlySales,
+//       backgroundColor: [
+//         'rgba(238, 184, 104, 1)',
+//         'rgba(75, 166, 223, 1)',
+//         'rgba(239, 118, 122, 1)',
+//       ],
+//       borderWidth: 0
+//     }]
+//   },
+//   options: {
+//     scales: {
+//       yAxes: [{
+//         ticks: {
+//           beginAtZero: true
+//         }
+//       }]
+//     }
+//   }
+// });
+
+// // Pie
+// var deptSalesChart = new Chart(pieCtx, {
+//   type: 'pie',
+//   data: {
+//     labels: deptLabels,
+//     datasets: [{
+//       label: '# of Sales',
+//       data: deptSales,
+//       backgroundColor: [
+//         'rgba(238, 184, 104, 1)',
+//         'rgba(75, 166, 223, 1)',
+//         'rgba(239, 118, 122, 1)',
+//       ],
+//       borderWidth: 0
+//     }]
+//   },
+//   options: {
+
+//   }
+// })
