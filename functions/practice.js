6b850c68ea6cd698ac370ad1f375d5d52b96d3f8
hanssenstijn <hanssenstijn@gmail.com>
2020-06-30 16:02:33 +0200

    practice file, basic functions

---
+++ functions/practice.js
@@ -0,0 +1,12 @@
+function greetings(name) {
+  console.log("Hello " + name);
+}
+
+greetings('John')
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+let result = sum(2, 3);
+console.log(result);
