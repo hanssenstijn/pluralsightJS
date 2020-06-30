44e2a74ef7bdb5190544bcb6fa6ce399194c6461
hanssenstijn <hanssenstijn@gmail.com>
2020-06-30 16:58:50 +0200

    out and in scope

--- functions/practice.js
+++ functions/practice.js
@@ -10,3 +10,48 @@ function sum(num1, num2) {
 
 let result = sum(2, 3);
 console.log(result);
+
+function printAll() {
+  for (let i = 0; i < arguments.length; i++) {
+    console.log(arguments[i]);
+  }
+}
+
+printAll(1, 2);
+
+function greet() {
+  return 'Hello my Friend!';
+}
+// have to asign to variable else lose return output
+let message = greet();
+console.log(message);
+
+// scope example
+let message2;
+
+function greeting() {
+  let message2 = "Yo";
+  return message2
+}
+x = greeting()
+console.log(x)
+
+function test() {
+  let testing = "working?"
+}
+test();
+// out of scope
+// console.log(testing);
+
+//change to var of usage out of scope
+let mes = 'hello';
+if (mes === 'hello') {
+  var count = 100;
+}
+console.log(count);
+
+// IIFE immediately Invoked Function Expression
+// (function () {
+//   statements
+// })();
+
